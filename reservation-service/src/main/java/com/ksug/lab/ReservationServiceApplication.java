915c4dd75442bc6a9183e12632bcd7bd7b649118
hspmuse <hspmuse@sk.com>
2016-07-12 18:22:39 +0900

    no message

--- reservation-service/src/main/java/com/ksug/lab/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/ksug/lab/ReservationServiceApplication.java
@@ -1,9 +1,14 @@
 package com.ksug.lab;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -12,23 +17,24 @@ import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
-import org.springframework.integration.annotation.MessageEndpoint;
-import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.data.rest.core.annotation.*;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceProcessor;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import java.util.Arrays;
 import java.util.Collection;
 
-@EnableBinding(Sink.class)
-@EnableDiscoveryClient
+//@EnableBinding(Sink.class)
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class ReservationServiceApplication {
+
+	private static Logger LOGGER = LoggerFactory.getLogger(
+		ReservationServiceApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationServiceApplication.class, args);
@@ -46,19 +52,42 @@ public class ReservationServiceApplication {
 			rr.findAll().forEach(System.out::println);
 		};
 	}
-}
 
-
-@MessageEndpoint
-class MessageReservationReceiver {
-
-	@ServiceActivator(inputChannel = Sink.INPUT)
-	public void acceptReservation(String rn) {
-		this.reservationRepository.save(new Reservation(rn));
+	@Bean
+	HealthIndicator healthIndicator() {
+		return () -> Health.status("I <3 Spring!").build();
 	}
 
-	@Autowired
-	private ReservationRepository reservationRepository;
+	@Component
+	@RepositoryEventHandler
+	public static class ReservationEventHandler {
+
+		@Autowired
+		private CounterService counterService;
+
+		@HandleAfterCreate
+		public void create(Reservation p) {
+			count("reservation.create", p);
+		}
+
+		@HandleAfterSave
+		public void save(Reservation p) {
+			count("reservations.save", p);
+			count("reservations." + p.getId() + ".save", p);
+		}
+
+		@HandleAfterDelete
+		public void delete(Reservation p) {
+			count("reservations.delete", p);
+		}
+
+		protected void count(String evt, Reservation p) {
+			this.counterService.increment(evt);
+			this.counterService.increment("meter." + evt);
+		}
+	}
+
+
 }
 
 @RefreshScope
@@ -69,8 +98,22 @@ class MessageRestController {
 	private String message;
 
 	@RequestMapping("/message")
-	String message() {
+	String getMessage() {
 		return this.message;
+	}
+}
+
+
+@Component
+class ReservationResourceProcessor implements ResourceProcessor<Resource<Reservation>> {
+
+	@Override
+	public Resource<Reservation> process(Resource<Reservation> reservationResource) {
+		Reservation reservation = reservationResource.getContent();
+		Long id = reservation.getId();
+		String url = "http://aws.images.com/" + id + ".jpg";
+		reservationResource.add(new Link(url, "profile-photo"));
+		return reservationResource;
 	}
 }
 
@@ -82,33 +125,3 @@ interface ReservationRepository extends JpaRepository<Reservation, Long> {
 }
 
 
-@Entity
-class Reservation {
-
-	@Id
-	@GeneratedValue
-	private Long id;
-	private String reservationName;
-
-	Reservation() {}
-
-	public Reservation(String reservationName) {
-		this.reservationName = reservationName;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public String getReservationName() {
-		return reservationName;
-	}
-
-	@Override
-	public String toString() {
-		return "Reservation{" +
-			"id=" + id +
-			", reservationName='" + reservationName + '\'' +
-			'}';
-	}
-}
